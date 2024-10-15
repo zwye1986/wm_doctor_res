fc7a28275ae63444f5f5cebb3458e4dacc802c7a
changzhichao <changzhichao>
2024-10-15 16:05:01 +0800

    1

--- pdsci2/src/main/java/com/pinde/sci/enums/res/ArmyTypeEnum.java
+++ pdsci2/src/main/java/com/pinde/sci/enums/res/ArmyTypeEnum.java
@@ -2,6 +2,7 @@ package com.pinde.sci.enums.res;
 
 import com.pinde.core.commom.enums.GeneralEnum;
 import com.pinde.core.util.EnumUtil;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -40,6 +41,9 @@ public enum ArmyTypeEnum implements GeneralEnum<String> {
     }
 
     public static String getNameById(String id) {
+        if(StringUtils.isEmpty(id)){
+            return "";
+        }
         return EnumUtil.getById(id, ArmyTypeEnum.class).getName();
     }
     public static final Map<String, String> map = new HashMap();
