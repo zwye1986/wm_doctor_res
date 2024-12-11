8aac9a87419a01681216abb318f025d67f1e8cdf
zhangwenyun <zhangwenyun@njpdkj.com>
2024-12-11 17:33:29 +0800

    fix code

--- pdapp/src/main/java/com/pinde/sci/dao/base/DoctorTeacherRecruitBatchMapper.java
+++ pdapp/src/main/java/com/pinde/sci/dao/base/DoctorTeacherRecruitBatchMapper.java
@@ -2,8 +2,9 @@ package com.pinde.sci.dao.base;
 
 import com.pinde.core.model.DoctorTeacherRecruitBatch;
 import com.pinde.core.model.DoctorTeacherRecruitBatchExample;
-import java.util.List;
 import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface DoctorTeacherRecruitBatchMapper {
     int countByExample(DoctorTeacherRecruitBatchExample example);
--- pdapp/src/main/java/com/pinde/sci/dao/base/DoctorTeacherRecruitMapper.java
+++ pdapp/src/main/java/com/pinde/sci/dao/base/DoctorTeacherRecruitMapper.java
@@ -2,8 +2,9 @@ package com.pinde.sci.dao.base;
 
 import com.pinde.core.model.DoctorTeacherRecruit;
 import com.pinde.core.model.DoctorTeacherRecruitExample;
-import java.util.List;
 import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface DoctorTeacherRecruitMapper {
     int countByExample(DoctorTeacherRecruitExample example);
