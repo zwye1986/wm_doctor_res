97bbdd409b263e409543acf10c78538d9ce58a0d
changzhichao <changzhichao>
2025-02-11 19:01:34 +0800

    http://zentao.njpdxx.com:5049/zentao/my-work-bug.html

--- pdsci2/src/main/java/com/pinde/sci/biz/res/impl/ResRecBizImpl.java
+++ pdsci2/src/main/java/com/pinde/sci/biz/res/impl/ResRecBizImpl.java
@@ -7546,6 +7546,34 @@ public class ResRecBizImpl implements IResRecBiz {
 	}
 
 	@Override
+	public String batchAudit(String auditResult, String docFlow, String processFlow, String recType) {
+		List<String> recTypes = new ArrayList<>();
+		recTypes.add(recType);
+		List<ResRec> recList=searchRecByProcessWithBLOBs(recTypes,processFlow,docFlow);
+
+		if(null != recList && recList.size() > 0){
+			List<String> recFlows = recList.stream().map(ResRec::getRecFlow).collect(Collectors.toList());
+			List<List<String>> partitionRecFlows = Lists.partition(recFlows , 100);
+			SysUser sysUser=GlobalContext.getCurrentUser();
+			ResRec rec = new ResRec();
+			rec.setAuditTime(DateUtil.getCurrDateTime());
+			rec.setAuditUserFlow(sysUser.getUserFlow());
+			rec.setAuditUserName(sysUser.getUserName());
+			rec.setAuditStatusId(auditResult);
+			rec.setAuditStatusName(RecStatusEnum.getNameById(auditResult));
+
+			for(List<String> flows : partitionRecFlows){
+				ResRecExample recExample = new ResRecExample();
+				ResRecExample.Criteria criteria = recExample.createCriteria();
+				criteria.andRecFlowIn(flows);
+				resRecMapper.updateByExampleSelective(rec, recExample);
+			}
+		}
+
+		return com.pinde.core.common.GlobalConstant.OPRE_SUCCESSED_FLAG;
+	}
+
+	@Override
 	public List<ResRec>  searchRecAndActivityByProcess(Map<String, Object> paramMap) {
 //		List<ResRec> resRecList;
 //		resRecList = campaignRegistryExtMapper.searchRecAndActivityByProcess(paramMap);
