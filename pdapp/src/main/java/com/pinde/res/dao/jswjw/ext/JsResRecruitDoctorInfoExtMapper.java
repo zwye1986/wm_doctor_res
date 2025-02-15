47f899ae6571d0d9f63798f683fe8f08a6b25095
zhangwenyun <zhangwenyun@njpdkj.com>
2025-02-15 14:05:53 +0800

    feat:优化SQL

--- pdapp/src/main/java/com/pinde/res/dao/jswjw/ext/JsResRecruitDoctorInfoExtMapper.java
+++ pdapp/src/main/java/com/pinde/res/dao/jswjw/ext/JsResRecruitDoctorInfoExtMapper.java
@@ -1,14 +1,11 @@
 package com.pinde.res.dao.jswjw.ext;
 
-import com.pinde.sci.form.JsRecruitDocInfoExt;
-import com.pinde.sci.form.JsresDoctorInfoExt;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 import java.util.Map;
 
 public interface JsResRecruitDoctorInfoExtMapper {
-	List<Map<String,Object>> searchRecruitDoctorInfos(Map<String, Object> param);
 
 	List<Map<String,Object>> zltjCityDoctorType(Map<String, Object> param);
 
@@ -25,9 +22,6 @@ public interface JsResRecruitDoctorInfoExtMapper {
 	List<Map<String,Object>> cityOrgListByOrgFlow(@Param("jointOrgFlowList") List<String> jointOrgFlowList);
 
 	List<Map<String,Object>> zltjOrgCityDoctorType(Map<String, Object> param);
-
-	List<JsRecruitDocInfoExt> searchRecruitDoctorInfosByResume(Map<String, Object> param);
-
 	/**
 	 * @Author shengl
 	 * @Description // 查询录取学员数据
@@ -40,10 +34,6 @@ public interface JsResRecruitDoctorInfoExtMapper {
 	List<Map<String,Object>> getRecruitSpeInfoNew(Map<String, Object> map);
 
 	List<Map<String,Object>> getRecruitSpeInfoNewAcc(Map<String, Object> map);
-
-    List<JsresDoctorInfoExt> searchRecruitDoctorInfosByResume2(Map<String, Object> param);
-
-	List<JsresDoctorInfoExt> searchRecruitDoctorInfosByResume3(Map<String, Object> param);
 
 	List<Map<String,Object>> zltjOrgLocalListNew(Map<String, Object> param);
 }
